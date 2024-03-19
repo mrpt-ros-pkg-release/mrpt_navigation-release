a7b73b26b8d808df8c4db2b3558248dbe218d6e7
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-03-19 20:24:32 +0100

    Rebase from 'upstream'

---
+++ mrpt_local_obstacles/submodules/mp2p_icp/mp2p_icp/src/Matcher.cpp
@@ -0,0 +1,81 @@
+/* -------------------------------------------------------------------------
+ *  A repertory of multi primitive-to-primitive (MP2P) ICP algorithms in C++
+ * Copyright (C) 2018-2021 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+/**
+ * @file   Matcher.cpp
+ * @brief  Pointcloud matching generic base class
+ * @author Jose Luis Blanco Claraco
+ * @date   June 22, 2020
+ */
+
+#include <mp2p_icp/Matcher.h>
+#include <mrpt/core/exceptions.h>
+
+IMPLEMENTS_VIRTUAL_MRPT_OBJECT(Matcher, mrpt::rtti::CObject, mp2p_icp)
+
+using namespace mp2p_icp;
+
+void Matcher::initialize(const mrpt::containers::yaml& params)
+{
+    runFromIteration = params.getOrDefault<uint32_t>("runFromIteration", 0);
+    runUpToIteration = params.getOrDefault<uint32_t>("runUpToIteration", 0);
+}
+
+bool Matcher::match(
+    const metric_map_t& pcGlobal, const metric_map_t& pcLocal,
+    const mrpt::poses::CPose3D& localPose, const MatchContext& mc,
+    MatchState& ms, Pairings& out) const
+{
+    if (mc.icpIteration < runFromIteration) return false;
+    if (runUpToIteration > 0 && mc.icpIteration > runUpToIteration)
+        return false;
+    return impl_match(pcGlobal, pcLocal, localPose, mc, ms, out);
+}
+
+Pairings mp2p_icp::run_matchers(
+    const matcher_list_t& matchers, const metric_map_t& pcGlobal,
+    const metric_map_t& pcLocal, const mrpt::poses::CPose3D& local_wrt_global,
+    const MatchContext&                   mc,
+    const mrpt::optional_ref<MatchState>& userProvidedMS)
+{
+    Pairings pairings;
+
+    MatchState* ms = nullptr;
+
+    std::optional<MatchState> localMS;
+
+    if (userProvidedMS.has_value())
+    {
+        // Use user-provided memory storage:
+        ms = &userProvidedMS.value().get();
+    }
+    else
+    {
+        // Reserve here:
+        localMS.emplace(pcGlobal, pcLocal);
+        ms = &localMS.value();
+    }
+
+    bool anyRun = false;
+
+    for (const auto& matcher : matchers)
+    {
+        ASSERT_(matcher);
+        Pairings pc;
+        bool     hasRun =
+            matcher->match(pcGlobal, pcLocal, local_wrt_global, mc, *ms, pc);
+        anyRun = anyRun || hasRun;
+        pairings.push_back(pc);
+    }
+
+    if (!anyRun)
+    {
+        std::cerr << "[mp2p_icp::run_matchers] WARNING: No active matcher "
+                     "actually ran on the two maps."
+                  << std::endl;
+    }
+
+    return pairings;
+}
---
+++ mrpt_local_obstacles/submodules/mp2p_icp/tests/test-mp2p_matcher_pt2pl.cpp
@@ -0,0 +1,174 @@
+/* -------------------------------------------------------------------------
+ *   A Modular Optimization framework for Localization and mApping  (MOLA)
+ * Copyright (C) 2018-2021 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+
+/**
+ * @file   test-mp2p_matcher_p2pl.cpp
+ * @brief  Unit tests for matcher
+ * @author Jose Luis Blanco Claraco
+ * @date   July 22, 2020
+ */
+
+#include <mp2p_icp/Matcher_Point2Plane.h>
+#include <mp2p_icp/Matcher_Points_DistanceThreshold.h>
+#include <mp2p_icp/metricmap.h>
+#include <mrpt/maps/CSimplePointsMap.h>
+
+static mrpt::maps::CSimplePointsMap::Ptr generateGlobalPoints()
+{
+    auto pts = mrpt::maps::CSimplePointsMap::Create();
+
+    // Plane:
+    for (int ix = 0; ix < 10; ix++)
+        for (int iy = 0; iy < 10; iy++)
+            pts->insertPoint(ix * 0.01f, 5.0f + iy * 0.01f, .0f);
+
+    // Plane:
+    for (int iy = 0; iy < 10; iy++)
+        for (int iz = 0; iz < 10; iz++)
+            pts->insertPoint(10.0f, iy * 0.01f, iz * 0.01f);
+
+    // Not a plane:
+    for (int ix = 0; ix < 10; ix++)
+        for (int iy = 0; iy < 10; iy++)
+            for (int iz = 0; iz < 10; iz++)
+                pts->insertPoint(20.0f + ix * 0.01f, iy * 0.01f, iz * 0.01f);
+
+    return pts;
+}
+
+static mrpt::maps::CSimplePointsMap::Ptr generateLocalPoints()
+{
+    auto pts = mrpt::maps::CSimplePointsMap::Create();
+
+    pts->insertPointFast(0.f, 0.f, 0.f);
+    pts->insertPointFast(2.f, 0.f, 0.f);
+
+    return pts;
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+{
+    try
+    {
+        mp2p_icp::metric_map_t pcGlobal;
+        pcGlobal.layers[mp2p_icp::metric_map_t::PT_LAYER_RAW] =
+            generateGlobalPoints();
+
+        mp2p_icp::metric_map_t pcLocal;
+        pcLocal.layers[mp2p_icp::metric_map_t::PT_LAYER_RAW] =
+            generateLocalPoints();
+
+        {
+            auto m = mp2p_icp::Matcher_Point2Plane::Create();
+
+            mrpt::containers::yaml p;
+            p["distanceThreshold"]   = 0.1;
+            p["searchRadius"]        = 0.1;
+            p["minimumPlanePoints"]  = 5.0;
+            p["knn"]                 = 5;
+            p["planeEigenThreshold"] = 0.1;
+
+            m->initialize(p);
+
+            {
+                // For pose: identity
+                mp2p_icp::Pairings   pairs;
+                mp2p_icp::MatchState ms(pcGlobal, pcLocal);
+                m->match(pcGlobal, pcLocal, {0, 0, 0, 0, 0, 0}, {}, ms, pairs);
+                ASSERT_(pairs.empty());
+            }
+
+            {
+                // For pose #1
+                mp2p_icp::Pairings   pairs;
+                mp2p_icp::MatchState ms(pcGlobal, pcLocal);
+                m->match(pcGlobal, pcLocal, {0, 5, 0, 0, 0, 0}, {}, ms, pairs);
+                ASSERT_EQUAL_(pairs.size(), 1U);
+                ASSERT_EQUAL_(pairs.paired_pt2pl.size(), 1U);
+            }
+
+            {
+                // For pose #2
+                mp2p_icp::Pairings   pairs;
+                mp2p_icp::MatchState ms(pcGlobal, pcLocal);
+                m->match(
+                    pcGlobal, pcLocal, {8.04, 0, 0.0, 0, 0, 0}, {}, ms, pairs);
+                ASSERT_EQUAL_(pairs.size(), 1U);
+                ASSERT_EQUAL_(pairs.paired_pt2pl.size(), 1U);
+
+                const auto& p0 = pairs.paired_pt2pl.at(0);
+
+                ASSERT_NEAR_(p0.pt_local.x, 2.0, 1e-3);
+                ASSERT_NEAR_(p0.pt_local.y, 0.0, 1e-3);
+                ASSERT_NEAR_(p0.pt_local.z, 0.0, 1e-3);
+
+                ASSERT_NEAR_(p0.pl_global.centroid.x, 10.0, 0.01);
+                ASSERT_NEAR_(p0.pl_global.centroid.y, 0.0, 0.01);
+                ASSERT_NEAR_(p0.pl_global.centroid.z, 0.0, 0.01);
+
+                // Plane equation: "x=10"  (Ax+By+Cz+D=0)
+                ASSERT_NEAR_(p0.pl_global.plane.coefs[0], 1.0, 1e-3);
+                ASSERT_NEAR_(p0.pl_global.plane.coefs[1], 0.0, 1e-3);
+                ASSERT_NEAR_(p0.pl_global.plane.coefs[2], 0.0, 1e-3);
+                ASSERT_NEAR_(p0.pl_global.plane.coefs[3], -10.0, 1e-3);
+            }
+
+            {
+                // For pose #3
+                mp2p_icp::Pairings   pairs;
+                mp2p_icp::MatchState ms(pcGlobal, pcLocal);
+                m->match(
+                    pcGlobal, pcLocal, {18.053, 0.05, 0.03, 0, 0, 0}, {}, ms,
+                    pairs);
+                ASSERT_EQUAL_(pairs.paired_pt2pl.size(), 0U);
+            }
+
+            {
+                // For pose #2 bis, NOT avoiding duplicated matches with another
+                // pt-to-pt matcher:
+                auto mPt2Pt =
+                    mp2p_icp::Matcher_Points_DistanceThreshold::Create();
+                mrpt::containers::yaml p2;
+                p2["threshold"]                      = 0.1;
+                p2["allowMatchAlreadyMatchedPoints"] = true;
+                mPt2Pt->initialize(p2);
+
+                const mp2p_icp::Pairings pairs = mp2p_icp::run_matchers(
+                    {m, mPt2Pt}, pcGlobal, pcLocal, {8.04, 0, 0.0, 0, 0, 0},
+                    {});
+
+                // std::cout << pairs.contents_summary() << std::endl;
+                ASSERT_EQUAL_(pairs.size(), 2U);
+                ASSERT_EQUAL_(pairs.paired_pt2pt.size(), 1U);
+                ASSERT_EQUAL_(pairs.paired_pt2pl.size(), 1U);
+            }
+
+            {
+                // For pose #2 tris, DO avoid duplicated matches with another
+                // pt-to-pt matcher:
+                auto mPt2Pt =
+                    mp2p_icp::Matcher_Points_DistanceThreshold::Create();
+                mrpt::containers::yaml p2;
+                p2["threshold"]                      = 0.1;
+                p2["allowMatchAlreadyMatchedPoints"] = false;
+                mPt2Pt->initialize(p2);
+
+                const mp2p_icp::Pairings pairs = mp2p_icp::run_matchers(
+                    {m, mPt2Pt}, pcGlobal, pcLocal, {8.04, 0, 0.0, 0, 0, 0},
+                    {});
+
+                // std::cout << pairs.contents_summary() << std::endl;
+                ASSERT_EQUAL_(pairs.size(), 1U);
+                ASSERT_EQUAL_(pairs.paired_pt2pl.size(), 1U);
+            }
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << mrpt::exception_to_str(e) << "\n";
+        return 1;
+    }
+}
