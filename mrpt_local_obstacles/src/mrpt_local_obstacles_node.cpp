bfe42e3f68b799fbf4640092c516ec18832740ee
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-03-19 20:24:37 +0100

    Rebase from 'upstream'

--- mrpt_local_obstacles/src/mrpt_local_obstacles_node.cpp
+++ mrpt_local_obstacles/src/mrpt_local_obstacles_node.cpp
@@ -1,6 +1,6 @@
 /***********************************************************************************
  * Revised BSD License *
- * Copyright (c) 2014-2015, Jose-Luis Blanco <jlblanco@ual.es> *
+ * Copyright (c) 2014-2023, Jose-Luis Blanco <jlblanco@ual.es> *
  * All rights reserved. *
  *                                                                                 *
  * Redistribution and use in source and binary forms, with or without *
@@ -30,25 +30,27 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. *
  ***********************************************************************************/
 
+#include <mp2p_icp_filters/FilterDecimateVoxels.h>
 #include <mrpt/config/CConfigFile.h>
 #include <mrpt/gui/CDisplayWindow3D.h>
 #include <mrpt/maps/COccupancyGridMap2D.h>
 #include <mrpt/maps/CSimplePointsMap.h>
 #include <mrpt/obs/CObservation2DRangeScan.h>
+#include <mrpt/obs/CObservationPointCloud.h>
 #include <mrpt/obs/CSensoryFrame.h>
 #include <mrpt/opengl/CGridPlaneXY.h>
 #include <mrpt/opengl/COpenGLScene.h>
 #include <mrpt/opengl/CPointCloud.h>
 #include <mrpt/opengl/stock_objects.h>
 #include <mrpt/ros1bridge/laser_scan.h>
-#include <mrpt/ros1bridge/point_cloud.h>
+#include <mrpt/ros1bridge/point_cloud2.h>
 #include <mrpt/ros1bridge/pose.h>
 #include <mrpt/system/CTimeLogger.h>
 #include <mrpt/system/string_utils.h>
 #include <nav_msgs/Odometry.h>
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
-#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/transform_listener.h>
 
@@ -77,16 +79,19 @@ class LocalObstaclesNode
 	TAuxInitializer m_auxinit;	//!< Just to make sure ROS is init first
 	ros::NodeHandle m_nh;  //!< The node handle
 	ros::NodeHandle m_localn;  //!< "~"
-	bool m_show_gui;
-	std::string m_frameid_reference;  //!< typ: "odom"
-	std::string m_frameid_robot;  //!< typ: "base_link"
-	std::string
-		m_topic_local_map_pointcloud;  //!< Default: "local_map_pointcloud"
-	std::string m_source_topics_2dscan;	 //!< Default: "scan,laser1"
-	double m_time_window;  //!< In secs (default: 0.2). This can't be smaller
-	//! than m_publish_period
-	double m_publish_period;  //!< In secs (default: 0.05). This can't be larger
-	//! than m_time_window
+
+	bool m_show_gui = true;
+
+	std::string m_frameid_reference = "odom";
+	std::string m_frameid_robot = "base_link";
+
+	std::string m_topic_local_map_pointcloud = "local_map_pointcloud";
+
+	std::string m_source_topics_2dscan = "scan,laser1";
+	std::string m_source_topics_pointclouds = "";
+
+	double m_time_window = 0.20;  //!< [s]can't be smaller than m_publish_period
+	double m_publish_period = 0.05;	 //!< [s]
 
 	ros::Timer m_timer_publish;
 
@@ -102,8 +107,14 @@ class LocalObstaclesNode
 	boost::mutex m_hist_obs_mtx;
 
 	/** The local maps */
-	CSimplePointsMap m_localmap_pts;
+	CSimplePointsMap::Ptr m_localmap_pts = CSimplePointsMap::Create();
 	// COccupancyGridMap2D m_localmap_grid;
+
+	/// Used for example to run voxel grid decimation, etc.
+	/// Refer to mp2p_icp docs
+	mp2p_icp_filters::FilterPipeline m_filter_pipeline;
+
+	std::string m_filter_output_layer_name;	 //!< mp2p_icp output layer name
 
 	mrpt::gui::CDisplayWindow3D::Ptr m_gui_win;
 
@@ -113,6 +124,9 @@ class LocalObstaclesNode
 
 	//!< Subscriber to 2D laser scans
 	std::vector<ros::Subscriber> m_subs_2dlaser;
+
+	//!< Subscriber to point cloud sensors
+	std::vector<ros::Subscriber> m_subs_pointclouds;
 
 	tf2_ros::Buffer m_tf_buffer;
 	tf2_ros::TransformListener m_tf_listener{m_tf_buffer};
@@ -232,6 +246,102 @@ class LocalObstaclesNode
 
 	}  // end onNewSensor_Laser2D
 
+	/** Callback: On new sensor data
+	 */
+	void onNewSensor_PointCloud(const sensor_msgs::PointCloud2::ConstPtr& pts)
+	{
+		CTimeLoggerEntry tle(m_profiler, "onNewSensor_PointCloud");
+
+		ros::Duration timeout(1.0);
+		// Get the relative position of the sensor wrt the robot:
+		geometry_msgs::TransformStamped sensorOnRobot;
+		try
+		{
+			CTimeLoggerEntry tle2(
+				m_profiler, "onNewSensor_PointCloud.lookupTransform_sensor");
+
+			sensorOnRobot = m_tf_buffer.lookupTransform(
+				m_frameid_robot, pts->header.frame_id, pts->header.stamp,
+				timeout);
+		}
+		catch (const tf2::TransformException& ex)
+		{
+			ROS_ERROR("%s", ex.what());
+			return;
+		}
+
+		// Convert data to MRPT format:
+		const mrpt::poses::CPose3D sensorOnRobot_mrpt = [&]() {
+			tf2::Transform tx;
+			tf2::fromMsg(sensorOnRobot.transform, tx);
+			return mrpt::ros1bridge::fromROS(tx);
+		}();
+
+		// In MRPT, CObservation2DRangeScan holds both: sensor data +
+		// relative pose:
+		auto obsPts = CObservationPointCloud::Create();
+		const auto ptsMap = mrpt::maps::CSimplePointsMap::Create();
+		obsPts->pointcloud = ptsMap;
+		obsPts->sensorPose = sensorOnRobot_mrpt;
+		mrpt::ros1bridge::fromROS(*pts, *ptsMap);
+
+		ROS_DEBUG(
+			"[onNewSensor_PointCloud] %u points, sensor pose on robot %s",
+			static_cast<unsigned int>(ptsMap->size()),
+			sensorOnRobot_mrpt.asString().c_str());
+
+		// Get sensor timestamp:
+		const double timestamp = pts->header.stamp.toSec();
+
+		// Get robot pose at that time in the reference frame, typ: /odom ->
+		// /base_link
+		mrpt::poses::CPose3D robotPose;
+		try
+		{
+			CTimeLoggerEntry tle3(
+				m_profiler, "onNewSensor_PointCloud.lookupTransform_robot");
+
+			geometry_msgs::TransformStamped robotTfStamp;
+			try
+			{
+				robotTfStamp = m_tf_buffer.lookupTransform(
+					m_frameid_reference, m_frameid_robot, pts->header.stamp,
+					timeout);
+			}
+			catch (const tf2::ExtrapolationException& ex)
+			{
+				ROS_ERROR("%s", ex.what());
+				return;
+			}
+
+			robotPose = [&]() {
+				tf2::Transform tx;
+				tf2::fromMsg(robotTfStamp.transform, tx);
+				return mrpt::ros1bridge::fromROS(tx);
+			}();
+
+			ROS_DEBUG(
+				"[onNewSensor_PointCloud] robot pose %s",
+				robotPose.asString().c_str());
+		}
+		catch (const tf2::TransformException& ex)
+		{
+			ROS_ERROR("%s", ex.what());
+			return;
+		}
+
+		// Insert into the observation history:
+		TInfoPerTimeStep ipt;
+		ipt.observation = obsPts;
+		ipt.robot_pose = robotPose;
+
+		m_hist_obs_mtx.lock();
+		m_hist_obs.insert(
+			m_hist_obs.end(), TListObservations::value_type(timestamp, ipt));
+		m_hist_obs_mtx.unlock();
+
+	}  // end onNewSensor_Laser2D
+
 	/** Callback: On recalc local map & publish it */
 	void onDoPublish(const ros::TimerEvent&)
 	{
@@ -268,7 +378,7 @@ class LocalObstaclesNode
 
 		// Build local map(s):
 		// -----------------------------------------------
-		m_localmap_pts.clear();
+		m_localmap_pts->clear();
 		mrpt::poses::CPose3D curRobotPose;
 		{
 			CTimeLoggerEntry tle2(m_profiler, "onDoPublish.buildLocalMap");
@@ -312,20 +422,40 @@ class LocalObstaclesNode
 				relPose.inverseComposeFrom(ipt.robot_pose, curRobotPose);
 
 				// Insert obs:
-				m_localmap_pts.insertObservationPtr(ipt.observation, relPose);
+				m_localmap_pts->insertObservationPtr(ipt.observation, relPose);
 
 			}  // end for
+		}
+
+		// Filtering:
+		mrpt::maps::CPointsMap::Ptr filteredPts;
+
+		if (!m_filter_pipeline.empty())
+		{
+			mp2p_icp::metric_map_t mm;
+			mm.layers[mp2p_icp::metric_map_t::PT_LAYER_RAW] = m_localmap_pts;
+			mp2p_icp_filters::apply_filter_pipeline(m_filter_pipeline, mm);
+
+			filteredPts = mm.point_layer(m_filter_output_layer_name);
+		}
+		else
+		{
+			filteredPts = m_localmap_pts;
 		}
 
 		// Publish them:
 		if (m_pub_local_map_pointcloud.getNumSubscribers() > 0)
 		{
-			sensor_msgs::PointCloudPtr msg_pts =
-				sensor_msgs::PointCloudPtr(new sensor_msgs::PointCloud);
-			msg_pts->header.frame_id = m_frameid_robot;
-			msg_pts->header.stamp = ros::Time(obs.rbegin()->first);
+			sensor_msgs::PointCloud2 msg_pts;
+			msg_pts.header.frame_id = m_frameid_robot;
+			msg_pts.header.stamp = ros::Time(obs.rbegin()->first);
 
-			mrpt::ros1bridge::toROS(m_localmap_pts, msg_pts->header, *msg_pts);
+			auto simplPts =
+				std::dynamic_pointer_cast<mrpt::maps::CSimplePointsMap>(
+					filteredPts);
+			ASSERT_(simplPts);
+
+			mrpt::ros1bridge::toROS(*simplPts, msg_pts.header, msg_pts);
 			m_pub_local_map_pointcloud.publish(msg_pts);
 		}
 
@@ -346,9 +476,15 @@ class LocalObstaclesNode
 				gl_obs->setName("obstacles");
 				scene->insert(gl_obs);
 
+				auto gl_rawpts = mrpt::opengl::CPointCloud::Create();
+				gl_rawpts->setName("raw_points");
+				gl_rawpts->setPointSize(1.0);
+				gl_rawpts->setColor_u8(TColor(0x00ff00));
+				scene->insert(gl_rawpts);
+
 				auto gl_pts = mrpt::opengl::CPointCloud::Create();
-				gl_pts->setName("points");
-				gl_pts->setPointSize(2.0);
+				gl_pts->setName("final_points");
+				gl_pts->setPointSize(3.0);
 				gl_pts->setColor_u8(TColor(0x0000ff));
 				scene->insert(gl_pts);
 
@@ -361,8 +497,11 @@ class LocalObstaclesNode
 			ROS_ASSERT(!!gl_obs);
 			gl_obs->clear();
 
-			auto gl_pts = mrpt::ptr_cast<mrpt::opengl::CPointCloud>::from(
-				scene->getByName("points"));
+			auto glRawPts = mrpt::ptr_cast<mrpt::opengl::CPointCloud>::from(
+				scene->getByName("raw_points"));
+
+			auto glFinalPts = mrpt::ptr_cast<mrpt::opengl::CPointCloud>::from(
+				scene->getByName("final_points"));
 
 			for (const auto& o : obs)
 			{
@@ -377,7 +516,8 @@ class LocalObstaclesNode
 				gl_obs->insert(gl_axis);
 			}  // end for
 
-			gl_pts->loadFromPointsMap(&m_localmap_pts);
+			glRawPts->loadFromPointsMap(m_localmap_pts.get());
+			glFinalPts->loadFromPointsMap(filteredPts.get());
 
 			m_gui_win->unlockAccess3DScene();
 			m_gui_win->repaint();
@@ -388,36 +528,50 @@ class LocalObstaclesNode
    public:
 	/**  Constructor: Inits ROS system */
 	LocalObstaclesNode(int argc, char** argv)
-		: m_auxinit(argc, argv),
-		  m_nh(),
-		  m_localn("~"),
-		  m_show_gui(true),
-		  m_frameid_reference("odom"),
-		  m_frameid_robot("base_link"),
-		  m_topic_local_map_pointcloud("local_map_pointcloud"),
-		  m_source_topics_2dscan("scan,laser1"),
-		  m_time_window(0.2),
-		  m_publish_period(0.05)
+		: m_auxinit(argc, argv), m_nh(), m_localn("~")
 	{
 		// Load params:
 		m_localn.param("show_gui", m_show_gui, m_show_gui);
+
 		m_localn.param(
 			"frameid_reference", m_frameid_reference, m_frameid_reference);
 		m_localn.param("frameid_robot", m_frameid_robot, m_frameid_robot);
+
 		m_localn.param(
 			"topic_local_map_pointcloud", m_topic_local_map_pointcloud,
 			m_topic_local_map_pointcloud);
+
 		m_localn.param(
 			"source_topics_2dscan", m_source_topics_2dscan,
 			m_source_topics_2dscan);
+		m_localn.param(
+			"source_topics_pointclouds", m_source_topics_pointclouds,
+			m_source_topics_pointclouds);
+
 		m_localn.param("time_window", m_time_window, m_time_window);
 		m_localn.param("publish_period", m_publish_period, m_publish_period);
 
 		ROS_ASSERT(m_time_window > m_publish_period);
 		ROS_ASSERT(m_publish_period > 0);
 
+		// Optional filter pipeline:
+		if (const auto fil =
+				m_localn.param<std::string>("filter_yaml_file", {});
+			!fil.empty())
+		{
+			m_filter_pipeline =
+				mp2p_icp_filters::filter_pipeline_from_yaml_file(fil);
+
+			m_filter_output_layer_name =
+				m_localn.param<std::string>("filter_output_layer_name", {});
+			ASSERTMSG_(
+				!m_filter_output_layer_name.empty(),
+				"'filter_yaml_file' param also requires "
+				"'filter_output_layer_name'");
+		}
+
 		// Init ROS publishers:
-		m_pub_local_map_pointcloud = m_nh.advertise<sensor_msgs::PointCloud>(
+		m_pub_local_map_pointcloud = m_nh.advertise<sensor_msgs::PointCloud2>(
 			m_topic_local_map_pointcloud, 10);
 
 		// Init ROS subs:
@@ -426,6 +580,10 @@ class LocalObstaclesNode
 		nSubsTotal += this->subscribeToMultipleTopics(
 			m_source_topics_2dscan, m_subs_2dlaser,
 			&LocalObstaclesNode::onNewSensor_Laser2D);
+
+		nSubsTotal += this->subscribeToMultipleTopics(
+			m_source_topics_pointclouds, m_subs_pointclouds,
+			&LocalObstaclesNode::onNewSensor_PointCloud);
 
 		ROS_INFO(
 			"Total number of sensor subscriptions: %u\n",
@@ -436,8 +594,8 @@ class LocalObstaclesNode
 			"sensory information!");
 
 		// Local map params:
-		m_localmap_pts.insertionOptions.minDistBetweenLaserPoints = 0;
-		m_localmap_pts.insertionOptions.also_interpolate = false;
+		m_localmap_pts->insertionOptions.minDistBetweenLaserPoints = 0;
+		m_localmap_pts->insertionOptions.also_interpolate = false;
 
 		// Init timers:
 		m_timer_publish = m_nh.createTimer(
