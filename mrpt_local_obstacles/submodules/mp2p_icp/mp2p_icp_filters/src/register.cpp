b96f504f178a477aa58c9163f7c99984fd38c76f
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-09-04 16:48:40 +0200

    Revert "Trimmed the branch to only the mrpt_localization sub directory"
    This reverts commit 4395e9a470c6c63136dcf44e096ba02e49d0c270.

---
+++ mrpt_local_obstacles/submodules/mp2p_icp/mp2p_icp_filters/src/register.cpp
@@ -0,0 +1,33 @@
+/* -------------------------------------------------------------------------
+ * A repertory of multi primitive-to-primitive (MP2P) ICP algorithms in C++
+ * Copyright (C) 2018-2021 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+/**
+ * @file   register.cpp
+ * @brief  RTTI registry
+ * @author Jose Luis Blanco Claraco
+ * @date   Jun 10, 2019
+ */
+
+#include <mp2p_icp_filters/FilterBoundingBox.h>
+#include <mp2p_icp_filters/FilterDecimateVoxels.h>
+#include <mp2p_icp_filters/FilterDeleteLayer.h>
+#include <mp2p_icp_filters/FilterEdgesPlanes.h>
+#include <mp2p_icp_filters/Generator.h>
+#include <mrpt/core/initializer.h>
+
+MRPT_INITIALIZER(register_mola_lidar_segmentation)
+{
+    using mrpt::rtti::registerClass;
+
+    // Generators:
+    registerClass(CLASS_ID(mp2p_icp_filters::Generator));
+
+    // Filters:
+    registerClass(CLASS_ID(mp2p_icp_filters::FilterBase));
+    registerClass(CLASS_ID(mp2p_icp_filters::FilterDecimateVoxels));
+    registerClass(CLASS_ID(mp2p_icp_filters::FilterDeleteLayer));
+    registerClass(CLASS_ID(mp2p_icp_filters::FilterEdgesPlanes));
+    registerClass(CLASS_ID(mp2p_icp_filters::FilterBoundingBox));
+}
